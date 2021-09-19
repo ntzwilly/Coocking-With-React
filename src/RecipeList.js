079ae4884c9ec8a3b0454a9b60a8038eb4db73fb
ntzwilly <ntzwilly@gmail.com>
2021-09-19 20:42:07 +0200

    Add RecipeList.js

---
+++ src/RecipeList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+export default function RecipeList({ recipes }) {
+
+  return 
+  <div>
+    (recipes.map(recipes => {
+      <recipe></recipe>
+     }))  
+  </div>
+    
+}
