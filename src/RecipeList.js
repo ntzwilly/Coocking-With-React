d7270382f4bf16a645d57928e4a8948d89781ae3
ntzwilly <ntzwilly@gmail.com>
2021-09-20 22:17:40 +0200

    Edit src/RecipeList.js

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -3,11 +3,14 @@ import Recipe from './Recipe'
 
 export default function RecipeList({ recipes }) {
 
-  return 
-  <div>
-    (recipes.map(recipes => {
-      <recipe></recipe>
-     }))  
-  </div>
-    
+  return (
+    <div>
+      {
+        recipes.map(recipe => {
+          return <Recipe key={recipe.id} {...recipe }/>
+        })
+      }
+    </div>
+  )
+
 }
