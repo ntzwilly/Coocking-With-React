cff5eb4a20615e3572d44d33cd351cbed23b5b62
ntzwilly <ntzwilly@gmail.com>
2021-09-26 22:23:27 +0200

    Add css style

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -3,7 +3,7 @@ import Ingredient from './Ingredient';
 
 export default function IngredientList({ingredients}) {
   const ingredientElements = ingredients.map(ingredient => {
-    return <Ingredient key={ingredient.id} {...Ingredient}/>
+    return <Ingredient key={ingredient.id} {...ingredient }/>
   })
   return (
     <div>
