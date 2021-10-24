707236050d4be727278599dd290df1e66c6b7447
ntzwilly <ntzwilly@gmail.com>
2021-10-24 12:05:18 +0200

    Recipe Add and Delete Button interactivity

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,11 +5,13 @@ import IngredientList from './IngredientList';
 export default function Recipe(props) {
 
   const { 
+    id,
     name, 
     cookTime, 
     servings, 
     instructions,
     ingredients, 
+    handleRecipeDelete,
   } = props;
 
   return (
@@ -18,7 +20,7 @@ export default function Recipe(props) {
         <h3 className='recipe__title'>{ name }</h3>
         <div>
           <button className='btn btn--primary mr-1'>Edit</button>
-          <button className='btn btn--danger'>Delete</button>
+          <button className='btn btn--danger' onClick={() => handleRecipeDelete(id)}>Delete</button>
         </div>
       </div>
       <div className='recipe__row'>
