0c911c9c9e298018b591eb1afba616aa0ea36137
ntzwilly <ntzwilly@gmail.com>
2021-10-24 13:01:45 +0200

    UseEffect Hook and LocalStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import RecipeList from './RecipeList';
 import '../css/app.css';
 import { v4 as uuidv4 } from 'uuid';
@@ -7,7 +7,17 @@ export const RecipeContext = React.createContext()
 
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes)
-  
+  const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes'
+
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if(recipeJSON !== null) setRecipes(JSON.parse(recipeJSON))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
+
   const recipeContextValue = {
     handleRecipeAdd,
     handleRecipeDelete
